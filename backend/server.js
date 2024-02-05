5c64adfc90f59f201b6942c8f790ef12c4dd4b85
Abhishek kumar <aabhi9478@gmail.com>
2024-02-05 19:30:45 +0530

    testing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/goals',(req,res)=>{
+    // res.send("Get goals")
+    res.json({message:'Get goals'})
+})
+app.listen(port,()=>console.log(`Server started at port ${port}`))
